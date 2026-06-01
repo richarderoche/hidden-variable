892dc36766c9bea845c7e4192b3b6a3be34d14fa
Richard <richard@infinite-productivity.com>
2026-05-31 17:16:11 -0700

    prefooter

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -2,6 +2,7 @@ import SiteWidth from '@/components/shared/SiteWidth'
 import {sanityFetch} from '@/sanity/lib/live'
 import {settingsQuery} from '@/sanity/lib/queries'
 import LogoLeft from './icons/LogoLeft'
+import LogoMotto from './icons/LogoMotto'
 import LogoRight from './icons/LogoRight'
 import LogoWordmark from './icons/LogoWordmark'
 
@@ -15,22 +16,27 @@ export default async function Footer() {
   const {text, email, subject} = footerMailto || {}
 
   return (
-    <footer className="bottom-0 py-gut mt-gut">
-      <SiteWidth className="flex justify-between items-center gap-gut">
-        {email && (
-          <div className="grow">
-            <a
-              className="inline-link"
-              href={`mailto:${email}${subject ? `?subject=${subject}` : ''}`}
-            >
-              {text || email}
-            </a>
+    <footer className="bottom-0 pb-gut-200 mt-gut">
+      <SiteWidth>
+        <div className="mt-gut-500 mb-gut-400">
+          <LogoMotto className="w-full h-auto" />
+        </div>
+        <div className="flex justify-between items-center gap-gut">
+          {email && (
+            <div className="grow">
+              <a
+                className="inline-link"
+                href={`mailto:${email}${subject ? `?subject=${subject}` : ''}`}
+              >
+                {text || email}
+              </a>
+            </div>
+          )}
+          <div className="flex items-center h-[1.25em]" aria-label="Hidden Variable" role="img">
+            <LogoLeft className="w-auto h-full" aria-hidden="true" />
+            <LogoWordmark className="w-auto h-full" aria-hidden="true" />
+            <LogoRight className="w-auto h-full" aria-hidden="true" />
           </div>
-        )}
-        <div className="flex items-center h-[1.25em]" aria-label="Hidden Variable" role="img">
-          <LogoLeft className="w-auto h-full" aria-hidden="true" />
-          <LogoWordmark className="w-auto h-full" aria-hidden="true" />
-          <LogoRight className="w-auto h-full" aria-hidden="true" />
         </div>
       </SiteWidth>
     </footer>
